bfa1a027fcd57f22a8cc9c0aa0dd2d68c2e3b7d8
Abhishek Rana <40822125+AbhishekRana23@users.noreply.github.com>
2020-01-12 19:53:19 +0530

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -592,7 +592,7 @@ function driveUploadCompleteCallback(err: string, gid: string, url: string, file
       }else{
         var rawurl = constants.INDEX_DOMAIN + fileName ;
       }
-      var indexurl = encodeURIComponent(rawurl) ;
+      var indexurl = encodeURIComponent(uri);
       finalMessage = `<a href='${url}'>${fileName}</a> (${fileSizeStr}) \n\n Index Link:\n<a href='${indexurl}'>${fileName}</a>`;
     } else {
       finalMessage = `<a href='${url}'>${fileName}</a> \nIndex Link: \n\n<a href='${indexurl}'>${fileName}</a>`;
